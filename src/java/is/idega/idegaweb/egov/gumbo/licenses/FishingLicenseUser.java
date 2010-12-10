3024b153c4232b38ac78f0b1982a90228322342a
Þórhallur Helgason <laddi@idega.com>
2010-12-10 19:57:45 +0000

    Using real webservice instead of mock

--- src/java/is/idega/idegaweb/egov/gumbo/licenses/FishingLicenseUser.java
+++ src/java/is/idega/idegaweb/egov/gumbo/licenses/FishingLicenseUser.java
@@ -29,7 +29,7 @@ import com.idega.util.text.Item;
 public class FishingLicenseUser extends DefaultSpringBean {
 	
 	@Autowired
-	@Qualifier(DOFWSClient.MOCK)
+	@Qualifier(DOFWSClient.WEB_SERVICE)
 	private DOFWSClient client;
 	
 	@Autowired
