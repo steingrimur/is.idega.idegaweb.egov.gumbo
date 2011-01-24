d037f85749d103344ce637f6036f8747f23577dc
civilis <civilis@vmacbookpro.(none)>
2011-01-24 14:06:16 +0200

    using company data instead of user data in licenses forms

--- src/java/is/idega/idegaweb/egov/gumbo/bpm/violation/ViolationService.java
+++ src/java/is/idega/idegaweb/egov/gumbo/bpm/violation/ViolationService.java
@@ -26,7 +26,7 @@ import com.idega.util.text.Item;
 public class ViolationService extends DefaultSpringBean {
 	
 	@Autowired
-	@Qualifier(DOFWSClient.MOCK)
+	@Qualifier(DOFWSClient.WEB_SERVICE)
 	private ViolationDataProvider violationDataProvider;
 	
 	@Autowired
