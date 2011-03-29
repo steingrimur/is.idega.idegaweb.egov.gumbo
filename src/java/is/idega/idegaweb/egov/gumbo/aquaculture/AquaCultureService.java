5f9b5e8cbe9df9b7745ba8abae939405cf1c9848
civilis <civilis@vmacbookpro.(none)>
2011-03-29 13:54:52 +0300

    aqua report

--- src/java/is/idega/idegaweb/egov/gumbo/aquaculture/AquaCultureService.java
+++ src/java/is/idega/idegaweb/egov/gumbo/aquaculture/AquaCultureService.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 import com.idega.core.business.DefaultSpringBean;
+import com.idega.util.CoreConstants;
+import com.idega.util.StringUtil;
 import com.idega.util.text.Item;
 
 @Service("aquaCultureService")
@@ -37,18 +39,55 @@ public class AquaCultureService extends DefaultSpringBean {
 		
 		final List<Item> items = new ArrayList<Item>();
 		
-		items.add(new Item("id1", "specie group 1"));
-		items.add(new Item("id2", "specie group 2"));
+		items.add(new Item("fiskur", "Fiskur"));
+		items.add(new Item("seidi", "Seiði"));
+		items.add(new Item("hrogn", "Hrogn"));
+		items.add(new Item("lindyr", "Lindýr"));
+		items.add(new Item("krabbadyr", "Krabbadýr"));
+		items.add(new Item("fari", "Þari"));
 		
 		return items;
 	}
 	
+	private static enum Aquamethods {
+		
+		sjokviar("Sjókvíar", "rumm", "Rúmm."), jardtjarnir("Jarðtjarnir", "m2",
+		        "m2"), kerogrennslisfrair("Ker og rennslisþrær", "rumm",
+		        "Rúmm."), botnraikt("Botnrækt", "m2", "m2"), raiktunmeflinum(
+		        "Ræktun með línum", "m", "m"), afrarafferfir("Aðrar aðferðir",
+		        "rumm", "Rúmm.");
+		
+		private final String label, unitId, unitLabel;
+		
+		private Aquamethods(String label, String unitId, String unitLabel) {
+			
+			this.label = label;
+			this.unitId = unitId;
+			this.unitLabel = unitLabel;
+		}
+		
+		public String getLabel() {
+			return label;
+		}
+		
+		public String getUnitId() {
+			return unitId;
+		}
+		
+		public String getUnitLabel() {
+			return unitLabel;
+		}
+	}
+	
 	public List<Item> getAquamethods() {
 		
-		final List<Item> items = new ArrayList<Item>();
+		final List<Item> items = new ArrayList<Item>(
+		        Aquamethods.values().length);
 		
-		items.add(new Item("id1", "aquamethod 1"));
-		items.add(new Item("id2", "aquamethod 2"));
+		for (Aquamethods meth : Aquamethods.values()) {
+			
+			items.add(new Item(meth.name(), meth.getLabel()));
+		}
 		
 		return items;
 	}
@@ -57,8 +96,9 @@ public class AquaCultureService extends DefaultSpringBean {
 		
 		final List<Item> items = new ArrayList<Item>();
 		
-		items.add(new Item("id1", "aquaEnvironment 1"));
-		items.add(new Item("id2", "aquaEnvironment 2"));
+		items.add(new Item("ferskvatn", "Ferskvatn"));
+		items.add(new Item("fullsalt", "Fullsalt"));
+		items.add(new Item("halfsalt", "Hálfsalt"));
 		
 		return items;
 	}
@@ -67,8 +107,17 @@ public class AquaCultureService extends DefaultSpringBean {
 		
 		final List<Item> items = new ArrayList<Item>();
 		
-		items.add(new Item("id1", "specie 1"));
-		items.add(new Item("id2", "specie 2"));
+		items.add(new Item("barri", "Barri"));
+		items.add(new Item("bleikja", "Bleikja"));
+		items.add(new Item("lax", "Lax"));
+		items.add(new Item("luda", "Lúða"));
+		items.add(new Item("regnbogasilungur", "Regnbogasilungur"));
+		items.add(new Item("senegalflura", "Senegalflúra"));
+		items.add(new Item("sandhverfa", "Sandhverfa"));
+		items.add(new Item("ufsi", "Ufsi"));
+		items.add(new Item("ysa", "Ýsa"));
+		items.add(new Item("forskur", "Þorskur"));
+		items.add(new Item("annaf", "Annað"));
 		
 		return items;
 	}
@@ -77,8 +126,10 @@ public class AquaCultureService extends DefaultSpringBean {
 		
 		final List<Item> items = new ArrayList<Item>();
 		
-		items.add(new Item("id1", "farm purp 1"));
-		items.add(new Item("id2", "farm purp 2"));
+		items.add(new Item("aframeldi", "Áframeldi"));
+		items.add(new Item("aleldi", "Aleldi"));
+		items.add(new Item("fiskraikt", "Fiskrækt"));
+		items.add(new Item("raiktun", "Ræktun"));
 		
 		return items;
 	}
@@ -87,8 +138,11 @@ public class AquaCultureService extends DefaultSpringBean {
 		
 		final List<Item> items = new ArrayList<Item>();
 		
-		items.add(new Item("id1", "status now 1"));
-		items.add(new Item("id2", "status now 2"));
+		items.add(new Item("slaigt", "Slægt"));
+		items.add(new Item("oslaigt", "Óslægt"));
+		items.add(new Item("lifandi", "Lifandi"));
+		items.add(new Item("unnif", "Unnið"));
+		items.add(new Item("ounnif", "Óunnið"));
 		
 		return items;
 	}
@@ -97,20 +151,25 @@ public class AquaCultureService extends DefaultSpringBean {
 		
 		final List<Item> items = new ArrayList<Item>();
 		
-		items.add(new Item("id1", "sold to 1"));
-		items.add(new Item("id2", "sold to 2"));
+		items.add(new Item("island", "Ísland"));
+		items.add(new Item("usa", "USA"));
+		items.add(new Item("noregur", "Noregur"));
+		
+		// TODO: add countries from table
 		
 		return items;
 	}
 	
 	public String getQuantityUnitByAquamethodId(String aquamethodId) {
 		
-		return "unit_" + aquamethodId;
+		return StringUtil.isEmpty(aquamethodId) ? CoreConstants.EMPTY
+		        : Aquamethods.valueOf(aquamethodId).getUnitId();
 	}
 	
 	public String getQuantityUnitOutputByAquamethodId(String aquamethodId) {
 		
-		return "Quantity unit by " + aquamethodId;
+		return StringUtil.isEmpty(aquamethodId) ? CoreConstants.EMPTY
+		        : Aquamethods.valueOf(aquamethodId).getUnitLabel();
 	}
 	
 	public String getQuantityUnitBySpeciesGroupId(String speciesGroupId) {
