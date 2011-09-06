2502b6ca0b3e19346a6eec9220de6eab2ff7009e
bluebottle <palli@idega.com>
2011-09-06 10:00:40 +0000

    Trying to cast Serializable to String to see if that makes any difference to the values shown

--- src/java/is/idega/idegaweb/egov/gumbo/IWBundleStarter.java
+++ src/java/is/idega/idegaweb/egov/gumbo/IWBundleStarter.java
@@ -1,14 +1,27 @@
 package is.idega.idegaweb.egov.gumbo;
 
+import is.idega.idegaweb.egov.gumbo.aquaculture.timer.AquaCultureStatisticsTimer;
+
 import com.idega.idegaweb.IWBundle;
 import com.idega.idegaweb.IWBundleStartable;
+import com.idega.util.timer.PastDateException;
+import com.idega.util.timer.TimerManager;
 
 public class IWBundleStarter implements IWBundleStartable {
 
 	public void start(IWBundle starterBundle) {
+		TimerManager mgr = new TimerManager();
+
+		try {
+			mgr.addTimer(1, false, new AquaCultureStatisticsTimer());
+
+			// mgr.addTimer(0, 10, -1, -1, -1, -1, new MentorAbsentTimer(),
+			// "MentorAbsentTimer");
+		} catch (PastDateException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void stop(IWBundle starterBundle) {
 	}
-
 }
