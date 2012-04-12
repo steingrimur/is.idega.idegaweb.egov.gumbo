6edddf5c4b2b0dec6324e20de441d2657dffe016
Valdas Žemaitis <valdas@idega.com>
2012-04-12 15:56:14 +0300

    Caching variable for aqua report year

--- src/java/is/idega/idegaweb/egov/gumbo/IWBundleStarter.java
+++ src/java/is/idega/idegaweb/egov/gumbo/IWBundleStarter.java
@@ -3,14 +3,40 @@ package is.idega.idegaweb.egov.gumbo;
 import is.idega.idegaweb.egov.gumbo.aquaculture.timer.AquaCultureStatisticsTimer;
 import is.idega.idegaweb.egov.gumbo.timer.FocalTimer;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.idega.idegaweb.IWBundle;
 import com.idega.idegaweb.IWBundleStartable;
+import com.idega.jbpm.data.VariableInstanceQuerier;
+import com.idega.util.expression.ELUtil;
 import com.idega.util.timer.PastDateException;
 import com.idega.util.timer.TimerManager;
 
 public class IWBundleStarter implements IWBundleStartable {
 
+	@Autowired
+	private VariableInstanceQuerier variablesQuerier;
+
+	VariableInstanceQuerier getVariablesQuerier() {
+		if (variablesQuerier == null)
+			ELUtil.getInstance().autowire(this);
+		return variablesQuerier;
+	}
+
+	@Override
 	public void start(IWBundle starterBundle) {
+		Thread variablesLoader = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				getVariablesQuerier().loadVariables(Arrays.asList(
+						GumboConstants.VARIABLE_AQUA_REPORT_YEAR
+				));
+			}
+		});
+		variablesLoader.start();
+
 		TimerManager mgr = new TimerManager();
 
 		try {
@@ -23,6 +49,7 @@ public class IWBundleStarter implements IWBundleStartable {
 		}
 	}
 
+	@Override
 	public void stop(IWBundle starterBundle) {
 	}
 }
